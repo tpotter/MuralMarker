50c9331a0b169a8cdb56d6786f67674ca5b18d47
Tom Potter <tompotter@Toms-MacBook-Air.local>
2026-09-06 17:55:16 -0400

    fix(map): import map markers to avoid broken images on build

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -13,7 +13,16 @@ import { Timeline } from "./Timeline";
 import { useMuralRange } from "../lib/useMuralRange";
 import { MuralPopup } from "./MuralPopup";
 import { DateDisplay } from "./DateDisplay";
+import L from "leaflet";
+import markerIcon from "leaflet/dist/images/marker-icon.png?url";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png?url";
+import markerShadow from "leaflet/dist/images/marker-shadow.png?url";
 
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
 interface MapProps {
   murals: MuralEntry[];
 }
