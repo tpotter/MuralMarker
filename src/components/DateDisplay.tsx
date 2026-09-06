9310eb8a421f14538d2da5c1ca9ee32833328811
Tom Potter <tompotter@Toms-MacBook-Air.local>
2026-09-05 20:51:32 -0400

    refactor(display): improves tablet and desktop views

--- src/components/DateDisplay.tsx
+++ src/components/DateDisplay.tsx
@@ -8,9 +8,9 @@ export const DateDisplay = () => {
   const month = getMonthLabel(timelineFilter.month);
 
   return (
-    <div className="absolute left-4 top-4 z-[1000] bg-neutral-100 p-2 rounded-xl">
+    <div className="absolute left-4 top-4 md:right-6 md:left-auto md:top-28 z-[1000] bg-neutral-100 p-2 rounded-xl">
       <h3>Display Date:</h3>
-      <h3 className="text-2xl">
+      <h3 className="text-2xl md:text-4xl lg:text-4xl">
         {month} {timelineFilter.year}
       </h3>
     </div>
