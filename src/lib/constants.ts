ea70a094cf72fe5b7043ca9d9fa0f0c93ebf1e70
Tom Potter <2440245+tpotter@users.noreply.github.com>
2026-09-24 13:23:01 -0400

    fix(map): improve bouds for easier usage

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,6 +5,11 @@ export const MAP_BOUNDS = [
   [39.01, -76.89],
 ] as LatLngBoundsLiteral;
 
+export const MAP_PAN_BOUNDS = [
+  [38.68, -77.27],
+  [39.11, -76.77],
+] as LatLngBoundsLiteral;
+
 export const PHOTO_BASE_URL = "https://images.muralmarker.com/";
 
 export const PHOTO_PLACEHOLDER = {
